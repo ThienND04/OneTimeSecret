ce743f1d64c0a587cffa22c6ad3438636a119713
ThienND04 <nguyenducthien808@gmail.com>
2025-07-05 00:33:59 +0700

    fix app

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const bodyParser = require('body-parser');
 const db = require('./config/db');
-const Route = require('./routes/index');
+const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
 db.connect();
 
-const app = express();
-app.use(morgan('combined'));
-app.use(cors());
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-Route(app);
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-module.exports = app;
